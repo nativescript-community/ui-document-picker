eb59c16ab096e9c3812b4198111e23c68660502e
Karl Schmaltz <karlschmaltz@gmail.com>
2021-12-03 05:02:55 -0700

    chore: remove js/ts files from packages

--- packages/ui-document-picker/index.common.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=index.common.js.map
--- packages/ui-document-picker/index.ios.d.ts
+++
@@ -1,3 +0,0 @@
-import { FilePickerOptions } from './index.common';
-export { FilePickerOptions };
-export declare function openFilePicker(params: FilePickerOptions): Promise<unknown>;
